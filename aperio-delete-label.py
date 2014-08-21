8df0b3ef54b2578b0dd8fd6279fb202714c51d8c
Benjamin Gilbert <bgilbert@backtick.net>
2014-08-21 00:21:46 -0400

    Switch to GPLv2

--- aperio-delete-label.py
+++ aperio-delete-label.py
@@ -6,17 +6,18 @@
 #  All rights reserved.
 #
 #  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as
-#  published by the Free Software Foundation, version 2.1.
+#  it under the terms of version 2 of the GNU General Public License as
+#  published by the Free Software Foundation.
 #
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Lesser General Public License for more details.
+#  GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU Lesser General Public
-#  License along with this program. If not, see
-#  <http://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA.
 #
 
 import string
